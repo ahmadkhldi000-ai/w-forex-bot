eaaaa647ee6b16d05512a216b6a688e50876a36a
W Forex Dev <dev@wforex.io>
2026-07-11 12:47:24 +0300

    🚀 Deploy frontend to GitHub Pages (2026-07-11 09:47)

---
+++ _next/static/ymdFJC8htntYwXkvbTO0H/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/w-forex-bot//_next/:path+",
+        "destination": "/w-forex-bot/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
